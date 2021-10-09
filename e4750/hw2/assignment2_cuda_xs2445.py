d6cd2aee436496ec2f339eb49cf9761966ad7861
xs2445 <xs2445@columbia.edu>
2021-10-08 20:44:10 -0400

    pycuda-update

--- e4750/hw2/assignment2_cuda_xs2445.py
+++ e4750/hw2/assignment2_cuda_xs2445.py
@@ -6,7 +6,7 @@ import numpy as np
 import math
 import time
 import matplotlib.pyplot as plt
-
+import sys
 
 class cudaCipher:
     def __init__(self):
@@ -19,14 +19,10 @@ class cudaCipher:
         # here as lambda functions. 
         # Quick lambda function to calculate grid dimensions
 
-        # Read the decipher text
-        self.text_de = open('deciphertext.txt','r').read()
-        self.length = len(self.text_de)
-        
         # define block and grid dimensions
         self.blocksize = 256
         self.BlockDim = (self.blocksize,1,1)
-        self.GridDim = (math.ceil(self.length/256),1,1)
+        # self.GridDim = (math.ceil(self.length/256),1,1)
         
         
         # kernel code wrapper
@@ -43,7 +39,7 @@ class cudaCipher:
                 else decrypted[i] = char(asc);
             }
         }
-        """        
+        """
 
         # Compile the kernel code when an instance
         # of this class is made.
@@ -66,18 +62,26 @@ class cudaCipher:
         # Get kernel function
         func_dcp = self.mod.get_function("Decrypt")
 
+        # culculate the gridDim
+        # print(type(sentence))
+        length = len(sentence)*sys.getsizeof(sentence)
+        # print(length)
+        GridDim = (math.ceil(length/self.blocksize),1,1)
+
         # Device memory allocation for input and output array(s)
             # convert string into np.chararaay
         sentence = np.char.asarray(sentence)
+        # sentence = unicode(sentence, "utf-8") 
         decrypted = np.empty_like(sentence)
             # record execution time with memory allocation
         start_alc.record()
         sentence_d = gpuarray.to_gpu(sentence)
         decrypted_d = gpuarray.to_gpu(decrypted)
-
+        # print(type(sentence))
+        
         # Record execution time and execute operation.
         start_cpt.record()
-        func_dcp(sentence_d, decrypted_d, np.int32(self.length), block=self.BlockDim, grid=self.GridDim)
+        func_dcp(sentence_d, decrypted_d, np.int32(length), block=self.BlockDim, grid=GridDim)
         end.record()
         
         # Wait for the event to complete
@@ -88,9 +92,15 @@ class cudaCipher:
         # Fetch result from device to host
         decrypted = decrypted_d.get()
         # Convert output array back to string
+        # decrypted = decrypted.decode('utf-8')
+        # decrypted = literal_eval(sentence)
+        # decrypted = str(decrypted)
+        # print(type(decrypted))
+        # decrypted = np.array2string(decrypted)
+        decrypted = np.ndarray.tolist(decrypted)
+        # print(type(decrypted))
 
         return decrypted, time_alc
-
     
     def pyCipher(self, sentence):
         """
@@ -104,7 +114,6 @@ class cudaCipher:
         start = time.time()
         # using ord() convert sting to ascii whose datatype is int
         text_asc = [ord(i) for i in sentence]
-        print(type(text_asc[1]))
         decrypted = ''
         # decrypt
         for asc in text_asc:
@@ -117,26 +126,28 @@ class cudaCipher:
                     decrypted += chr(asc-13)
             else:
                 decrypted += chr(asc)
+        # print(type(decrypted))
         return decrypted, time.time()-start
 
 
-if __name__ == "__main__":
+
+def main():
     # Main code
     # create a instance of cudaCipher
     cipher = cudaCipher()
-    
+
     # Open text file to be deciphered.
     # Preprocess the file to separate sentences
     text_de = open('deciphertext.txt','r').read()
-    
+
     # Split string into list populated with '.' as delimiter.
     sentences = text_de.split('. ')
 
     # Empty lists to hold deciphered sentences, execution times
     decrypted_c = []
     decrypted_p = []
-    tc = 0
-    tp = 0
+    tc = []
+    tp = []
 
     # Loop over each sentence in the list
     for sentence in sentences:
@@ -144,10 +155,8 @@ if __name__ == "__main__":
         temp_sp, temp_tp = cipher.pyCipher(sentence)
         decrypted_c.append(temp_sc)
         decrypted_p.append(temp_sp)
-        tc += temp_tc
-        tp += temp_tp
-    tc /= len(sentences)
-    tp /= len(sentences)
+        tc.append(temp_tc)
+        tp.append(temp_tp)
 
     # post process the string(s) if required
     decrypted_cuda = []
@@ -156,37 +165,44 @@ if __name__ == "__main__":
             a = i
         decrypted_cuda.append(a)
 
-    # Execution time
-    print("CUDA output cracked in ", tc, " milliseconds per sentence.")
-    print("Python output cracked in ", tp, " milliseconds per sentence.")
+    tc = np.array(tc)
+    tp = np.array(tp)
 
-    # Error check
+    # Execution time
+    print("CUDA output cracked in ", tc.mean(), " milliseconds per sentence.")
+    print("Python output cracked in ", tp.mean(), " milliseconds per sentence.")
+
+    # check if the results match
     try:
         print("Checkpoint: Do python and kernel decryption match? Checking...")
-        assert # something
-        
-    except _________:
-        print("Checkpoint failed: Python and CUDA kernel decryption do not match. Try Again!")
-        # dump bad output to file for debugging
-        
-
-    # If ciphers agree, proceed to write decrypted text to file
-    # and plot execution times
-    
-    if #conditions met:
+        for i in range(len(decrypted_cuda)):
+            # print(decrypted_cuda[i], decrypted_p[i])
+            decrypted_cuda[i] += '. '
+            decrypted_p[i] += '. '
+            assert decrypted_cuda[i] == decrypted_p[i]
+    # dump bad output to file for debugging
+    except AssertionError:
+        print('Checkpoint failed: Python and CUDA kernel decryption do not match. Try Again!')
+    # If ciphers agree, proceed to write decrypted text to file and plot execution times
+    else: 
         print("Checkpoint passed!")
         print("Writing decrypted text to file...")
-
         # Write cuda output to file
-        
+        decrypted_text = open('decrypted_text.txt', 'w')
+        decrypted_text.write(''.join(decrypted_cuda))
+        decrypted_text.close()
         # Dot plot the  per-sentence execution times
+        plt.figure()
+        plt.scatter(range(len(decrypted_cuda)), tc, label='pycuda')
+        plt.scatter(range(len(decrypted_cuda)), tp, label='vanilla python')
+        plt.legend()
+        plt.grid()
+        plt.title('PyCUDA, Comparison of processing time (including memory allocation)')
+        plt.xlabel('Sentences')
+        plt.ylabel('Processing Time (ms)')
+        # plt.show()
+        plt.savefig('comparison_cuda.jpg')
 
 
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
