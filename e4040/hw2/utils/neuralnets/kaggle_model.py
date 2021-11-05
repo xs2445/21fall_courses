ab0fbec3d2aa8bd15a13126214ac0f6dbae06712
xs2445 <xs2445@columbia.edu>
2021-11-05 21:35:28 +0000

    updated-e4040-hw2-task5

---
+++ e4040/hw2/utils/neuralnets/kaggle_model.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import os
+import tensorflow as tf
+from tensorflow.keras.layers import Dense,GlobalAveragePooling2D
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam, RMSprop, SGD
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, BatchNormalization, Input, Dropout
+from tensorflow.keras import Model
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.losses import categorical_crossentropy
+import datetime
+from keras.callbacks import EarlyStopping
+from keras.activations import softmax
+from keras.activations import elu
+from keras.activations import relu
+from keras.layers.pooling import GlobalAveragePooling2D
+from keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.applications import ResNet50
+# from tensorflow.keras.applications.resnet50 import preprocess_input
+
+from tensorflow.keras.applications.efficientnet import EfficientNetB0
+from tensorflow.keras.applications.efficientnet import preprocess_input
+
+
+def create_model(lr = 1e-3):
+    #*****************************
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=3, activation='relu', padding='valid', input_shape=(128,128,3)))
+    model.add(BatchNormalization())
+    # model.add(Conv2D(32, kernel_size=3, activation='relu', padding='valid'))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Dropout(0.5))
+
+    model.add(Conv2D(256, kernel_size=3, activation='relu', padding='valid'))
+    model.add(BatchNormalization())
+    # model.add(Conv2D(256, kernel_size=3, activation='relu', padding='valid'))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Dropout(0.5))
+
+    model.add(Conv2D(64, kernel_size=3, activation='relu', padding='valid'))
+    model.add(BatchNormalization())
+    # model.add(Conv2D(64, kernel_size=3, activation='relu', padding='valid'))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Dropout(0.5))
+
+    # model.add(Conv2D(128, kernel_size=3, activation='relu', padding='same'))
+    # model.add(BatchNormalization())
+    # model.add(Conv2D(128, kernel_size=3, activation='relu', padding='same'))
+    # model.add(BatchNormalization())
+    # model.add(MaxPooling2D())
+    # model.add(Dropout(0.4))
+
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(Dropout(0.4))
+    model.add(Dense(5, activation='softmax'))
+    #*****************************
+
+    
+    # for layers in model_efn.layers:
+    # layers.trainable = True
+
+
+    # optimizer = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True) # Adam(lr=lr, decay=0.01) 
+    optimizer = Adam(learning_rate=lr, decay=0.01)
+
+    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
+
+
+def create_model2(lr=1e-3):
+    
+    #********************
+    base_model=MobileNet(weights='imagenet',include_top=False) 
+    x=base_model.output
+    x=GlobalAveragePooling2D()(x)
+    # x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
+    # x=Dense(1024,activation='relu')(x) #dense layer 2
+    x=Dense(512,activation='relu')(x) #dense layer 3
+    preds=Dense(5,activation='softmax')(x) #final layer with softmax activation
+    model=Model(inputs=base_model.input,outputs=preds)
+    #********************
+
+    
+    for layer in model.layers[:20]:
+        layer.trainable=False
+    for layer in model.layers[20:]:
+        layer.trainable=True
+        
+    optimizer = Adam(learning_rate=lr, decay=0.01)
+        
+    model.compile(optimizer=optimizer,loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    
+    return model
