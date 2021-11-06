4c0950cc953d928cbfab2c870ba93278ff122990
xs2445 <xs2445@columbia.edu>
2021-11-06 21:32:50 +0000

    updated-e4040-hw2-task5

--- e4040/hw2/utils/neuralnets/kaggle_model.py
+++ e4040/hw2/utils/neuralnets/kaggle_model.py
@@ -20,6 +20,13 @@ from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.applications.efficientnet import EfficientNetB0
 from tensorflow.keras.applications.efficientnet import preprocess_input
 
+from tensorflow.keras.applications.efficientnet import EfficientNetB7
+from tensorflow.keras.applications.efficientnet import preprocess_input
+
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+
 
 def create_model(lr = 1e-3):
     #*****************************
@@ -80,22 +87,21 @@ def create_model2(lr=1e-3):
     base_model=EfficientNetB0(weights='imagenet',include_top=False, input_shape=(128,128,3)) 
     x=base_model.output
     
-    x = GlobalAveragePooling2D()(x)
+    # x = GlobalAveragePooling2D()(x)
+    x = Flatten()(x)
 
     x = Dropout(0.5)(x)
 
     x = BatchNormalization()(x)
 
-    
-    # model.add(GlobalAveragePooling2D())
-
-    # x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
     x=Dense(512,activation='relu')(x) #dense layer 1
     x = BatchNormalization()(x)
+    
     x=Dense(512,activation='relu')(x) #dense layer 2
     x = BatchNormalization()(x)
     x = Dropout(0.5)(x)
-    x=Dense(64,activation='relu')(x) #dense layer 3
+    
+    # x=Dense(64,activation='relu')(x) #dense layer 3
     preds=Dense(5,activation='softmax')(x) #final layer with softmax activation
     model=Model(inputs=base_model.input,outputs=preds)
     #********************
@@ -116,7 +122,7 @@ def create_model2(lr=1e-3):
     return model
 
 
-def create_model2(lr=1e-3):
+def create_model3(lr=1e-3):
     
     #********************
     base_model=ResNet50(weights='imagenet',include_top=False, input_shape=(128,128,3)) 
@@ -130,22 +136,176 @@ def create_model2(lr=1e-3):
     
     x=Dense(1024,activation='relu')(x) #dense layer 1
     x = BatchNormalization()(x)
-    # x=Dense(512,activation='relu')(x) #dense layer 2
-    # x = BatchNormalization()(x)
-    # x = Dropout(0.5)(x)
+    x = Dropout(0.5)(x)
+
+
     x=Dense(512,activation='relu')(x) #dense layer 3
     preds=Dense(5,activation='softmax')(x) #final layer with softmax activation
     model=Model(inputs=base_model.input,outputs=preds)
     #********************
 
+    trainable_layers = 18
     
-    # for layer in model.layers[:20]:
-    #     layer.trainable=False
-    # for layer in model.layers[20:]:
-    #     layer.trainable=True
+    for layer in model.layers[:trainable_layers]:
+        layer.trainable=False
+    for layer in model.layers[trainable_layers:]:
+        layer.trainable=True
     
-    for layers in model.layers:
-        layers.trainable = True   
+    # for layers in model.layers:
+    #     layers.trainable = True   
+        
+    optimizer = Adam(learning_rate=lr, decay=0.01)
+        
+    model.compile(optimizer=optimizer,loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    
+    return model
+
+
+def create_model4(lr=1e-3):
+    
+    #********************
+    base_model=EfficientNetB7(weights='imagenet',include_top=False, input_shape=(128,128,3)) 
+    x=base_model.output
+    
+    x = GlobalAveragePooling2D()(x)
+
+    x = Dropout(0.5)(x)
+
+    x = BatchNormalization()(x)
+
+    x=Dense(512,activation='relu')(x) #dense layer 1
+    x = BatchNormalization()(x)
+    
+    x=Dense(512,activation='relu')(x) #dense layer 2
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+    
+    x=Dense(64,activation='relu')(x) #dense layer 3
+    preds=Dense(5,activation='softmax')(x) #final layer with softmax activation
+    
+    model=Model(inputs=base_model.input,outputs=preds)
+    #********************
+
+    trainable_layers = 18
+    
+    for layer in model.layers[:trainable_layers]:
+        layer.trainable=False
+    for layer in model.layers[trainable_layers:]:
+        layer.trainable=True
+    
+    # for layers in model.layers:
+    #     layers.trainable = True   
+        
+    optimizer = Adam(learning_rate=lr, decay=0.01)
+        
+    model.compile(optimizer=optimizer,loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    
+    return model
+
+
+
+def create_model5(lr=1e-3):
+    
+    #********************
+    base_model=VGG16(weights='imagenet',include_top=False, input_shape=(128,128,3)) 
+    x=base_model.output
+    
+    x = Flatten()(x)
+    
+    # x = GlobalAveragePooling2D()(x)
+
+    x = Dropout(0.5)(x)
+
+    x = BatchNormalization()(x)
+
+    x=Dense(512,activation='relu')(x) #dense layer 1
+    x = BatchNormalization()(x)
+    
+    x=Dense(512,activation='relu')(x) #dense layer 2
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+    
+    x=Dense(64,activation='relu')(x) #dense layer 3
+    preds=Dense(5,activation='softmax')(x) #final layer with softmax activation
+    
+    model=Model(inputs=base_model.input,outputs=preds)
+    #********************
+
+    untrainable_layers = 5
+    
+    for layer in model.layers[:untrainable_layers]:
+        layer.trainable=False
+    for layer in model.layers[untrainable_layers:]:
+        layer.trainable=True
+    
+    # for layers in model.layers:
+    #     layers.trainable = True   
+        
+    optimizer = Adam(learning_rate=lr, decay=0.01)
+        
+    model.compile(optimizer=optimizer,loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    
+    return model
+
+
+def create_model6(lr=1e-3):
+    
+    #********************
+    base_model=EfficientNetB0(weights='imagenet',include_top=False, input_shape=(128,128,3)) 
+    x=base_model.output
+    
+    x = GlobalAveragePooling2D()(x)
+
+    x = Dropout(0.5)(x)
+
+    preds=Dense(5,activation='softmax')(x) #final layer with softmax activation
+    
+    model=Model(inputs=base_model.input,outputs=preds)
+    #********************
+
+    untrainable_layers = 5
+    
+    for layer in model.layers[:untrainable_layers]:
+        layer.trainable=False
+    for layer in model.layers[untrainable_layers:]:
+        layer.trainable=True
+    
+    # for layers in model.layers:
+    #     layers.trainable = True   
+        
+    optimizer = Adam(learning_rate=lr, decay=0.01)
+        
+    model.compile(optimizer=optimizer,loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    
+    return model
+
+
+def create_model7(lr=1e-3):
+    
+    #********************
+    base_model=ResNet50(weights='imagenet',include_top=False, input_shape=(128,128,3)) 
+    x=base_model.output
+    
+    x = GlobalAveragePooling2D()(x)
+
+    x = Dropout(0.5)(x)
+
+    x = BatchNormalization()(x)
+
+    preds=Dense(5,activation='softmax')(x) #final layer with softmax activation
+    
+    model=Model(inputs=base_model.input,outputs=preds)
+    #********************
+
+    trainable_layers = 0
+    
+    for layer in model.layers[:trainable_layers]:
+        layer.trainable=False
+    for layer in model.layers[trainable_layers:]:
+        layer.trainable=True
+    
+    # for layers in model.layers:
+    #     layers.trainable = True   
         
     optimizer = Adam(learning_rate=lr, decay=0.01)
         
